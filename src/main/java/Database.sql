0cb1faa646dce306cd061c90c593cc4f1dea5cf9
PHanse <peterhansendev@gmail.com>
2022-08-26 14:57:26 +0200

    Update SQL

--- src/main/java/Database.sql
+++ src/main/java/Database.sql
@@ -6,4 +6,4 @@ insert into customer(customer_id, first_name, last_name, country, postal_code, p
 update customer set first_name = 'banana' where customer_id = 1;
 select max(country) from customer;
 select invoice_id, customer_id, total from invoice where total = (select max(total) from invoice);
-select customer_id, first_name, last_name, country, postal_code, phone, email from customer where exists (select * from invoice where total = (select max(total) from invoice) order by total);
+--select customer_id, first_name, last_name, country, postal_code, phone, email from customer where exists (select * from invoice where total = (select max(total) from invoice) order by total);
